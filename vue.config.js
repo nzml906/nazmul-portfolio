26d2b6edeedad349f39da24e401e2dc3ddc73883
Nazmul <grey000906@gmail.com>
2020-05-06 00:02:20 +0600

    projects app

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/nz-portfolio/' : '/',
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/nazmul-portfolio/' : '/',
   pluginOptions: {
     i18n: {
       locale: 'us',
