920a129763e8610cb3149d9502aad5cb59f98cc6
Nazmul Ahmed <grey000906@gmail.com>
2021-09-20 18:05:51 +0600

    Update skills.js

--- src/data/skills.js
+++ src/data/skills.js
@@ -14,27 +14,22 @@ const skills = [
   {
     id: '8',
     title: 'React.js',
-    skill: ['React Hooks', 'MATERIAL-UI']
+    skill: ['Gatsby', 'Graphql', 'Next Js', 'React Hooks', 'MATERIAL-UI']
   },
   {
     id: '7',
     title: 'Node.js',
-    skill: ['MongoDB', 'Express js', 'RESTful APIs', 'authentication', 'JWT']
+    skill: ['API', 'Express js', 'RESTful APIs', 'authentication', 'JWT']
   },
   {
     id: '3',
     title: 'Database',
-    skill: ['MongoDB', 'Firebase']
+    skill: ['MongoDB', 'Firestore', 'mysql']
   },
   {
     id: '1',
-    title: 'CSS',
-    skill: ['Mobile First Design', 'Bootstrap 4', 'Sass']
-  },
-  {
-    id: '2',
-    title: 'JAVASCRIPT',
-    skill: ['OOP Programming', 'ES6+']
+    title: 'CSS Lirary',
+    skill: ['Tailwind CSS', 'Bootstrap 5']
   }
 ];
 
