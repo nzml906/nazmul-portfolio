5666e7cb06fcbff6070f71f411d379a060896980
Nazmul Ahmed <grey000906@gmail.com>
2021-09-21 15:15:42 +0600

    deploy portfolio

---
+++ precache-manifest.bfe5e2ff2ae11bc91ead451eced6ebd7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5349cbb00533e7363be",
+    "url": "/nazmul-portfolio/css/app.c113bad0.css"
+  },
+  {
+    "revision": "54200b0f54c90da6e1f8",
+    "url": "/nazmul-portfolio/css/chunk-vendors.ac0834e4.css"
+  },
+  {
+    "revision": "35ed4fb89081d8e33e6ce604d911b93b",
+    "url": "/nazmul-portfolio/index.html"
+  },
+  {
+    "revision": "a5349cbb00533e7363be",
+    "url": "/nazmul-portfolio/js/app.9921c9e0.js"
+  },
+  {
+    "revision": "cc5099953a6a8f9f0ebd",
+    "url": "/nazmul-portfolio/js/chunk-4a3fdb38.b11cdeb2.js"
+  },
+  {
+    "revision": "e6f811aab7967ffa6214",
+    "url": "/nazmul-portfolio/js/chunk-4a404d9e.b69887d4.js"
+  },
+  {
+    "revision": "f0fcbd250e9c51cb6afc",
+    "url": "/nazmul-portfolio/js/chunk-4a41dc8b.9c50518d.js"
+  },
+  {
+    "revision": "a25bcddd1ae371566ef3",
+    "url": "/nazmul-portfolio/js/chunk-4a56ca57.be27b3de.js"
+  },
+  {
+    "revision": "54200b0f54c90da6e1f8",
+    "url": "/nazmul-portfolio/js/chunk-vendors.9b49cbc9.js"
+  },
+  {
+    "revision": "d3ec0b85cd88f7c847a2631d8b79fe6c",
+    "url": "/nazmul-portfolio/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/nazmul-portfolio/robots.txt"
+  }
+]);
