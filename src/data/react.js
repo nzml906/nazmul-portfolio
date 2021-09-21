9aad36813da263bd5b875b5ca7912bfa292e3628
Nazmul Ahmed <grey000906@gmail.com>
2021-09-21 15:14:40 +0600

    react new projects

--- src/data/react.js
+++ src/data/react.js
@@ -1,5 +1,21 @@
 const projects = [
   {
+    img: 'https://i.imgur.com/IhguKsY.jpg',
+    id: '4',
+    titulo: 'Gatsby site',
+    linkGithub: 'https://github.com/nzml906/gatsby_01',
+    linkSitio: 'https://gatsbytour.gatsbyjs.io/',
+    skills: ['react js', 'gatsby', 'pre-rendering', 'SSG', 'SSR']
+  },
+  {
+    img: 'https://i.imgur.com/wFZgaN1.png',
+    id: '3',
+    titulo: 'Next Js blog',
+    linkGithub: 'https://github.com/nzml906/neo_next',
+    linkSitio: 'https://neo-next.vercel.app/',
+    skills: ['react js', 'next js', 'pre-rendering', 'SSG', 'SSR']
+  },
+  {
     img: 'https://i.imgur.com/nskjLXC.png',
     id: '1',
     titulo: 'COVID-19-react',
@@ -21,6 +37,14 @@ const projects = [
     linkGithub: 'https://github.com/nzml906/react-ToDo',
     linkSitio: 'https://nzml906.github.io/react-ToDo/',
     skills: ['react js', 'javascript', 'material-ui']
+  },
+  {
+    img: 'https://i.imgur.com/p6wRDQl.jpg',
+    id: '5',
+    titulo: 'emoji-search',
+    linkGithub: 'https://github.com/nzml906/react-emoji-search',
+    linkSitio: 'https://boring-booth-af175e.netlify.app/',
+    skills: ['react js', 'javascript']
   }
 ];
 
